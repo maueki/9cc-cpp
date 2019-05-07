994db62afa321587b24b969224a3c47eef5b0c0e
Ueki Masaru <masaru.ueki@itage.co.jp>
2019-05-07 12:23:00 +0900

    step1

---
+++ test.sh
@@ -0,0 +1,33 @@
+##!/bin/bash
+try() {
+  expected="$1"
+  input="$2"
+
+  ./build/9cc "$input" > tmp.s
+  gcc -o tmp tmp.s
+  ./tmp
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+build() {
+  pushd .
+  mkdir -p build
+  cd build
+  cmake ..
+  make
+  popd
+}
+
+cd "$(dirname "$0")"
+build
+try 0 0
+try 42 42
+
+echo OK
